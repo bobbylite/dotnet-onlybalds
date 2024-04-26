25d747c18103ca20fe5d9c23f804fa925a45caec
Bobby <bobbielite@pm.me>
2024-04-26 01:25:45 -0400

    Added support for jquery
    Added additional styling

--- OnlyBalds.Client/Components/Pages/ChatRoom.razor.js
+++ OnlyBalds.Client/Components/Pages/ChatRoom.razor.js
@@ -1,4 +1,18 @@
 export function initializeChatroomReferences(dotnetHelper) {
+    $(document).ready(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
     dotnetHelper.invokeMethodAsync('Connect');
   };
 
+export function OnEnterKeyPressed(dotnetHelper, user, message) {
+    $(document).ready(function() {
+        $(document).keypress(function(e) {
+            if (e.which == 13) {
+                $("input").blur();
+                dotnetHelper.invokeMethodAsync('Send', user, message);
+            }
+        });
+    });
+}
