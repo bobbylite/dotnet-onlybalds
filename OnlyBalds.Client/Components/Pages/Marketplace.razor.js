9af82f7c2ff0b5d8421792a23983a272d26062b6
Bobby <bobbielite@pm.me>
2024-04-29 18:17:39 -0400

    Adding support for ecwid shop

--- OnlyBalds.Client/Components/Pages/Marketplace.razor.js
+++ OnlyBalds.Client/Components/Pages/Marketplace.razor.js
@@ -1,5 +1,28 @@
 export const onLoad = () => {
-    $(document).ready(function() {
-        $('.navbar-collapse').collapse('hide');
-    });
+    $('.navbar-collapse').collapse('hide');
+}
+
+export const downloadMarketplace = (elementId, dotnetHelper) => {
+    var scriptUrl = 'https://app.ecwid.com/script.js?103074754&data_platform=code&data_date=2024-04-29';
+    var scripts = Array.from(document.getElementsByTagName('script'));
+    var scriptElement = scripts.find(s => s.src === scriptUrl);
+
+    if (!scriptElement) {
+        var script = document.createElement('script');
+        script.src = scriptUrl;
+        script.charset = 'utf-8';
+        script.setAttribute('data-cfasync', 'false');
+        var container = document.getElementById(elementId);
+        container.appendChild(script);
+        
+        script.onload = function() {
+            xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-103074754");
+            dotnetHelper.invokeMethod('CompleteDownloading');
+        };
+    } else {
+        xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-103074754");
+        dotnetHelper.invokeMethod('CompleteDownloading');
+    }
+
+    console.log("Marketplace downloaded.");
 }
