b39318f0d077a41cef12d53c6275e274c79b92e9
Bobby <bobbielite@pm.me>
2025-08-01 15:57:38 -0400

    Adding redirect if account has not submitted questionnaire

--- OnlyBalds/Endpoints/IdentityEndpoints.cs
+++ OnlyBalds/Endpoints/IdentityEndpoints.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using OnlyBalds.Client.Models;
+
 namespace OnlyBalds.Endpoints;
 
 /// <summary>
@@ -14,27 +17,49 @@ public static class IdentityEndpoints
     {
         ArgumentNullException.ThrowIfNull(endpoints);
 
-        endpoints.MapGet("/identity-information", async context =>
-        {
-            var user = context.User;
-
-            if (user?.Identity is not { IsAuthenticated: true })
-            {
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                return;
-            }
-
-            var claims = user.Claims
-                .ToDictionary(claim => claim.Type, claim => claim.Value);
-
-            await context.Response.WriteAsJsonAsync(new
-            {
-                IsAuthenticated = user.Identity.IsAuthenticated,
-                Name = user.Identity.Name,
-                Claims = claims
-            });
-        }).RequireAuthorization();
+        endpoints
+        .MapGet("/identity-information", GetIdentityInformation)
+        .RequireAuthorization();
 
         return endpoints;
+    }
+
+    private static async Task GetIdentityInformation(
+        HttpContext context,
+        [FromServices] IHttpClientFactory httpClientFactory
+    )
+    {
+        ArgumentNullException.ThrowIfNull(context);
+        ArgumentNullException.ThrowIfNull(httpClientFactory);
+
+        var httpClient = httpClientFactory.CreateClient(HttpClientNames.OnlyBalds);
+        var subject = context.User?.FindFirst("sub")?.Value;
+        var response = await httpClient.GetFromJsonAsync<AccountItem>($"account?id={Uri.EscapeDataString(subject!)}");
+
+        if (response is null)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            return;
+        }
+
+        var user = context.User;
+
+        if (user?.Identity is not { IsAuthenticated: true })
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return;
+        }
+
+        var claims = user.Claims
+            .ToDictionary(claim => claim.Type, claim => claim.Value);
+
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsJsonAsync(new
+        {
+            Account = response,
+            IsAuthenticated = user.Identity.IsAuthenticated,
+            Name = user.Identity.Name,
+            Claims = claims
+        });
     }
 }
