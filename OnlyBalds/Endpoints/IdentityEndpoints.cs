e55b51ae71142893f2c5d0fa21721b7fe15dc9ff
Bobby <bobbielite@pm.me>
2025-08-02 02:16:26 -0400

    Bugfix

--- OnlyBalds/Endpoints/IdentityEndpoints.cs
+++ OnlyBalds/Endpoints/IdentityEndpoints.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using OnlyBalds.Client.Models;
 
@@ -32,31 +34,51 @@ public static class IdentityEndpoints
         ArgumentNullException.ThrowIfNull(context);
         ArgumentNullException.ThrowIfNull(httpClientFactory);
 
-        var httpClient = httpClientFactory.CreateClient(HttpClientNames.OnlyBalds);
-        var subject = context.User?.FindFirst("sub")?.Value;
-        var response = await httpClient.GetFromJsonAsync<AccountItem>($"account?id={Uri.EscapeDataString(subject!)}");
-
-        if (response is null)
-        {
-            context.Response.StatusCode = StatusCodes.Status404NotFound;
-            return;
-        }
-
         var user = context.User;
 
         if (user?.Identity is not { IsAuthenticated: true })
         {
-            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new
+            {
+                IsAuthenticated = user?.Identity?.IsAuthenticated,
+            });
+
             return;
         }
 
         var claims = user.Claims
             .ToDictionary(claim => claim.Type, claim => claim.Value);
 
-        context.Response.ContentType = "application/json";
+        var httpClient = httpClientFactory.CreateClient(HttpClientNames.OnlyBalds);
+        var subject = context.User?.FindFirst("sub")?.Value;
+        var accountsResponse = await httpClient.GetAsync($"account?id={Uri.EscapeDataString(subject!)}");
+        AccountItem? account = null;
+
+        if (accountsResponse.StatusCode is HttpStatusCode.NotFound)
+        {
+            await context.Response.WriteAsJsonAsync(new
+            {
+                Account = account,
+                IsAuthenticated = user.Identity.IsAuthenticated,
+                Name = user.Identity.Name,
+                Claims = claims
+            });
+
+            return;
+        }
+
+        account = await accountsResponse.Content.ReadFromJsonAsync<AccountItem>();
+
+        if (account is null)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            return;
+        }
+
         await context.Response.WriteAsJsonAsync(new
         {
-            Account = response,
+            Account = account,
             IsAuthenticated = user.Identity.IsAuthenticated,
             Name = user.Identity.Name,
             Claims = claims
